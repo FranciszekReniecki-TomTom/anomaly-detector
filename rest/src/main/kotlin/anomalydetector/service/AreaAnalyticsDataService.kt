f293887e5fc4647ceabee85ede763fda43d39bb6
FranciszekReniecki-TomTom <franciszek.reniecki@tomtom.com>
2025-07-28 17:26:21 +0200

    feat: [AD-005] simple fetch function for traffic data

---
+++ rest/src/main/kotlin/anomalydetector/service/AreaAnalyticsDataService.kt
@@ -0,0 +1,73 @@
+package anomalydetector.service
+
+import anomalydetector.model.TileHour
+import com.tomtom.tti.area.analytics.io.storage.AreaAnalyticsStorage
+import com.tomtom.tti.area.analytics.model.traffic.Traffic
+import com.tomtom.tti.area.analytics.model.traffic.aggregate
+import com.tomtom.tti.nida.morton.geom.MortonTileLevel
+import com.tomtom.tti.nida.storage.processingTileFromCode
+import io.github.cdimascio.dotenv.Dotenv
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import org.locationtech.jts.geom.Geometry
+import java.time.LocalDate
+
+private val storage = AreaAnalyticsStorage(
+  account = Dotenv.load()["AREA_ANALYTICS_ACCOUNT_NAME"],
+  key = Dotenv.load()["AREA_ANALYTICS_KEY"],
+)
+
+fun getData(
+  days: Int,
+  startDay: LocalDate,
+  tile: Long,
+  level: MortonTileLevel<*>,
+  geometry: Geometry
+): List<TileHour> = runBlocking {
+  generateSequence(startDay) { it.plusDays(1) }
+    .take(days)
+    .map { day ->
+      async(Dispatchers.Default) {
+        day to getDay(day, tile, level, geometry)
+      }
+    }
+    .toList()
+    .awaitAll()
+    .flatMap { (date, trafficList) ->
+      trafficList.map { traffic ->
+        TileHour(date, traffic.hour, traffic.id, traffic.traffic)
+      }
+    }
+}
+
+private suspend fun getDay(
+  day: LocalDate,
+  tile: Long = 3597,
+  level: MortonTileLevel<*>,
+  geometry: Geometry
+): List<AggregatedTraffic> = storage.traffic
+  .read(day, processingTileFromCode(tile), level, geometry)
+  .awaitAll()
+  .flatten()
+  .flatMap { it.m20Traffic }
+  .groupBy { (Pair(it.id, it.hour)) }
+  .map { (key, group) ->
+    val (id, hour) = key
+    val aggregatedTraffic = group.map { it.traffic }.aggregate()
+    AggregatedTraffic(
+      id = id,
+      hour = hour,
+      traffic = aggregatedTraffic.rounded()
+    )
+  }
+  .toList()
+
+private data class AggregatedTraffic(
+  val id: Long,
+  val hour: Byte,
+  val traffic: Traffic
+)
+
+
