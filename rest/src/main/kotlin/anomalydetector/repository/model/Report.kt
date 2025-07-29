7cb8678cc66e5d1ac89ab955b0e7e1b938e4ddb0
PawelWieczorek-TomTom <pawelwieczorek@tomtom.com>
2025-07-29 12:45:57 +0200

    feat: [AD-003] implemented PR suggestioins

--- rest/src/main/kotlin/anomalydetector/repository/model/Report.kt
+++ rest/src/main/kotlin/anomalydetector/repository/model/Report.kt
@@ -3,14 +3,15 @@ package anomalydetector.repository.model
 import jakarta.persistence.*
 import java.time.LocalDateTime
 import java.util.*
-import org.locationtech.jts.geom.Geometry
+import org.locationtech.jts.geom.Polygon
 
 @Entity
 data class Report(
         @Id @GeneratedValue val id: UUID? = null,
         val name: String,
         @Column(name = "creation_date") val creationDate: LocalDateTime,
-        @Column(columnDefinition = "geometry") val geometry: Geometry? = null,
+        @Column(columnDefinition = "geometry(Polygon,4326)")
+        val geometry: Polygon? = null,
         @OneToMany(mappedBy = "report", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
         val clusters: List<Cluster> = emptyList()
 )
