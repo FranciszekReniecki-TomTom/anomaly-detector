9542c4ead663fb0a963346b3024e46cca1dc9db8
FranciszekReniecki-TomTom <franciszek.reniecki@tomtom.com>
2025-08-01 11:43:15 +0200

    feat: [AD-002] model adjustment do LocalDateTime

--- rest/src/main/kotlin/anomalydetector/model/TrafficTileHour.kt
+++ rest/src/main/kotlin/anomalydetector/model/TrafficTileHour.kt
@@ -1,14 +1,31 @@
 package anomalydetector.model
 
 import com.tomtom.tti.area.analytics.model.traffic.Traffic
-import java.time.LocalDate
+import com.tomtom.tti.nida.morton.geom.MortonTileLevel
+import java.time.LocalDateTime
 
 /**
- * Representation of a space-time object given Morton tile in given day and hour
+ * Representation of a space-time object: Morton tile in given day and hour
  *
- * @property date The date of the tile hour.
- * @property hour The hour of the day (0-23).
+ * @property datetime The date and time of the tile hour.
  * @property id The Morton Tile identifier for the tile hour.
+ * @property level The Morton tile level of the tile hour.
  * @property traffic Traffic data associated with the tile hour.
  */
-data class TrafficTileHour(val date: LocalDate, val hour: Byte, val id: Long, val traffic: Traffic)
+data class TrafficTileHour(
+    val datetime: LocalDateTime,
+    val id: Long,
+    val level: MortonTileLevel<*>,
+    val traffic: Traffic,
+) {
+    fun geoTime(): DoubleArray {
+        val tile = level.getTile(id)
+        val hour = datetime.dayOfYear * 24 + datetime.hour
+        return doubleArrayOf(tile.lat, tile.lon, hour.toDouble())
+    }
+
+    fun stringify(): String {
+        val tile = level.getTile(id)
+        return "TTH: $datetime [$id (${tile.lat}, ${tile.lon})], traffic=$traffic"
+    }
+}
