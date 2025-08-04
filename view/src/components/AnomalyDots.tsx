0bd430531ed6f590a6e103d24bd77c0a2813dcc1
Jakub Klepacz <jakub.klepacz@tomtom.com>
2025-08-04 16:09:46 +0200

    feat: [AD-008] added grid to anomalies chart

--- view/src/components/AnomalyDots.tsx
+++ view/src/components/AnomalyDots.tsx
@@ -41,11 +41,130 @@ export default function AnomalyDots({
     return <div ref={containerRef} style={{ width: "100%" }} />;
   }
 
+  const generateTimeGridLines = () => {
+    const gridLines: React.ReactElement[] = [];
+    const totalDuration = maxTime - minTime;
+    const timeStep = totalDuration / 10;
+
+    for (let i = 0; i <= 10; i++) {
+      const time = minTime + timeStep * i;
+      const leftPercent = getLeftPercent(time);
+
+      gridLines.push(
+        <div
+          key={`vgrid-${i}`}
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: `${leftPercent}%`,
+            width: 1,
+            backgroundColor: "#e0e0e0",
+            pointerEvents: "none",
+            zIndex: 1,
+          }}
+        />
+      );
+    }
+    return gridLines;
+  };
+
+  const generateLaneGridLines = () => {
+    const gridLines: React.ReactElement[] = [];
+
+    for (let i = 0; i < anomalyIds.length; i++) {
+      const topPos = baseLaneHeight * i;
+
+      gridLines.push(
+        <div
+          key={`hgrid-${i}`}
+          style={{
+            position: "absolute",
+            top: topPos,
+            left: 0,
+            right: 0,
+            height: 1,
+            backgroundColor: "#e0e0e0",
+            pointerEvents: "none",
+            zIndex: 1,
+          }}
+        />
+      );
+    }
+
+    gridLines.push(
+      <div
+        key="hgrid-bottom"
+        style={{
+          position: "absolute",
+          top: height,
+          left: 0,
+          right: 0,
+          height: 1,
+          backgroundColor: "#e0e0e0",
+          pointerEvents: "none",
+          zIndex: 1,
+        }}
+      />
+    );
+
+    return gridLines;
+  };
+
+  const generateDateLabels = () => {
+    const labels: React.ReactElement[] = [];
+    const totalDuration = maxTime - minTime;
+    const timeStep = totalDuration / 10;
+
+    for (let i = 0; i <= 10; i++) {
+      const time = minTime + timeStep * i;
+      const leftPercent = getLeftPercent(time);
+      const date = new Date(time);
+
+      const dateLabel = date.toLocaleDateString([], {
+        month: "short",
+        day: "numeric",
+      });
+      const timeLabel = date.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+
+      labels.push(
+        <div
+          key={`date-${i}`}
+          style={{
+            position: "absolute",
+            left: `${leftPercent}%`,
+            bottom: -35,
+            transform: "translateX(-50%)",
+            fontSize: 9,
+            color: "#666",
+            whiteSpace: "nowrap",
+            pointerEvents: "none",
+            zIndex: 2,
+            textAlign: "center",
+            lineHeight: "1.2",
+          }}
+        >
+          <div>{dateLabel}</div>
+          <div>{timeLabel}</div>
+        </div>
+      );
+    }
+    return labels;
+  };
+
   return (
     <div
       ref={containerRef}
-      style={{ width: "100%", position: "relative", height }}
+      style={{ width: "100%", position: "relative", height, paddingBottom: 40 }}
     >
+      {generateTimeGridLines()}
+      {generateLaneGridLines()}
+
+      {generateDateLabels()}
+
       <div
         style={{
           position: "absolute",
@@ -60,6 +179,7 @@ export default function AnomalyDots({
           zIndex: 9999,
         }}
       />
+
       {timestamps.map(({ time, anomaly_id }, i) => {
         const laneIndex = anomalyIds.indexOf(anomaly_id);
         const leftPercent = getLeftPercent(time);
@@ -79,6 +199,7 @@ export default function AnomalyDots({
               backgroundColor: "#de1c12",
               cursor: "default",
               transform: "translate(-50%, -50%)",
+              zIndex: 100,
             }}
           />
         );
