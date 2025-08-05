2706c0e8caa9e0a63902e658a46381f3145af416
PawelWieczorek-TomTom <pawelwieczorek@tomtom.com>
2025-08-05 16:47:16 +0200

    feat: [AD-007] filling missing data with nulls

--- rest/src/main/kotlin/anomalydetector/dto/AnomalyRequestDto.kt
+++ rest/src/main/kotlin/anomalydetector/dto/AnomalyRequestDto.kt
@@ -17,22 +17,3 @@ data class AnomalyRequestDto(
     @field:NotEmpty @field:Size(min = 3) val polygon: List<List<Double>>,
     @field:NotNull val dataType: DataType,
 )
-
-// data class Coordinate(
-//    @field:NotNull
-//    val latitude: Double,
-//    @field:NotNull
-//    val longitude: Double,
-// )
-//
-// fun List<Coordinate>.toPolygon(geometryFactory: GeometryFactory = GeometryFactory()): Polygon =
-//    let { coords ->
-//        val closed =
-//            if (coords.isNotEmpty() && coords.first() != coords.last()) {
-//                coords + coords.first()
-//            } else {
-//                coords
-//            }
-//        val jtsCoords = closed.map { JtsCoordinate(it.longitude, it.latitude) }.toTypedArray()
-//        geometryFactory.createPolygon(geometryFactory.createLinearRing(jtsCoords), null)
-//    }
--- rest/src/main/kotlin/anomalydetector/dto/ReportDto.kt
+++ rest/src/main/kotlin/anomalydetector/dto/ReportDto.kt
@@ -1,5 +1,5 @@
 package anomalydetector.dto
 
-import anomalydetector.controller.FeatureCollection
+import anomalydetector.service.FeatureCollection
 
 data class ReportDto(val name: String, val hourToClassIdToPolygon: FeatureCollection)
