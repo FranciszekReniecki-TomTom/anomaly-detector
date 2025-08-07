142be98644ac1f2eca22cb34cdb3447e9e5d27c0
FranciszekReniecki-TomTom <franciszek.reniecki@tomtom.com>
2025-08-07 11:56:03 +0200

    feat: [AD-024] labeling request dto with retrieving geoTime points

--- rest/src/main/kotlin/anomalydetector/dto/AnomalyLabelRequestDto.kt
+++ rest/src/main/kotlin/anomalydetector/dto/AnomalyLabelRequestDto.kt
@@ -1,3 +1,22 @@
 package anomalydetector.dto
 
-data class AnomalyLabelRequestDto(val name: String)
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AnomalyLabelRequestDto(
+    val type: String = "Feature Collection",
+    val features: List<GeoJsonFeature>
+)
+
+@Serializable
+data class GeoJsonFeature(
+    val type: String = "Feature",
+    val geometry: GeoJsonPolygon,
+    val properties: Map<String, String>
+)
+
+@Serializable
+data class GeoJsonPolygon(
+    val type: String = "Polygon",
+    val coordinates: List<List<List<Double>>>
+)
