2c29bfb917be90ef20a08a7b466ad87d578633ea
PawelWieczorek-TomTom <pawelwieczorek@tomtom.com>
2025-08-07 10:36:45 +0200

    feat: [AD-007] fixed polygon ingore bug

--- rest/src/main/kotlin/anomalydetector/service/AnomalyService.kt
+++ rest/src/main/kotlin/anomalydetector/service/AnomalyService.kt
@@ -1,9 +1,30 @@
 package anomalydetector.service
 
+import anomalydetector.dto.DataType
+import anomalydetector.model.TrafficTileHour
+import anomalydetector.model.engine.findClusters
+import anomalydetector.model.engine.findWeeklyOutliers
+import anomalydetector.service.trafficdata.getData
+import com.tomtom.tti.nida.morton.geom.MortonTileLevel
+import com.tomtom.tti.nida.morton.geometry
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
+import kotlin.collections.toDoubleArray
+import org.locationtech.jts.geom.Coordinate as JtsCoordinate
+import org.locationtech.jts.geom.GeometryFactory
+import org.locationtech.jts.geom.Polygon
+import anomalydetector.controller.FeatureCollection
+import anomalydetector.controller.toPolygon
 import anomalydetector.dto.AnomalyLabelRequestDto
 import anomalydetector.dto.AnomalyRequestDto
+import anomalydetector.dto.DataType
 import anomalydetector.dto.LabelDto
 import anomalydetector.dto.ReportDto
+import anomalydetector.model.TrafficTileHour
+import anomalydetector.model.engine.findClusters
+import anomalydetector.service.detection.findOutliers
 import anomalydetector.service.labeling.AnomalySliceHour
 import anomalydetector.service.labeling.GeoTime
 import anomalydetector.service.labeling.LlmLabelingService
@@ -17,6 +38,19 @@ import org.locationtech.jts.geom.Coordinate
 import org.locationtech.jts.geom.GeometryFactory
 import org.locationtech.jts.geom.Polygon
 import org.springframework.stereotype.Service
+
+data class FeatureCollection(
+    val type: String = "FeatureCollection",
+    val features: List<GeoJsonFeature>,
+)
+
+data class GeoJsonFeature(
+    val type: String = "Feature",
+    val geometry: GeoJsonPolygon,
+    val properties: Map<String, Any>,
+)
+
+data class GeoJsonPolygon(val type: String = "Polygon", val coordinates: List<List<List<Double>>>)
 
 @Service
 class AnomalyService(
