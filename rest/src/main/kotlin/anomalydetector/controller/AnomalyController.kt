fa49221fa19cb32f0c03e60ccd9e5083baf7942a
FranciszekReniecki-TomTom <franciszek.reniecki@tomtom.com>
2025-08-07 10:23:34 +0200

    feat: [AD-024] rest API for anomaly labeling

--- rest/src/main/kotlin/anomalydetector/controller/AnomalyController.kt
+++ rest/src/main/kotlin/anomalydetector/controller/AnomalyController.kt
@@ -1,6 +1,8 @@
 package anomalydetector.controller
 
+import anomalydetector.dto.AnomalyLabelRequestDto
 import anomalydetector.dto.AnomalyRequestDto
+import anomalydetector.dto.LabelDto
 import anomalydetector.dto.ReportDto
 import anomalydetector.service.AnomalyService
 import io.swagger.v3.oas.annotations.Operation
@@ -13,10 +15,16 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/anomaly")
 class AnomalyController(private val anomalyService: AnomalyService) {
-  @Operation(summary = "Detect anomalies in provided data and time range")
-  @PostMapping
-  fun detectAnomaly(@RequestBody request: AnomalyRequestDto): ResponseEntity<ReportDto> {
-    return ResponseEntity.ok(anomalyService.detectAnomaly(request))
-  }
+    @Operation(summary = "Detect anomalies in provided data and time range")
+    @PostMapping
+    fun detectAnomaly(@RequestBody request: AnomalyRequestDto): ResponseEntity<ReportDto> {
+        return ResponseEntity.ok(anomalyService.detectAnomaly(request))
+    }
+
+    @Operation(summary = "Labels given anomaly")
+    @PostMapping("/label")
+    fun labelAnomaly(@RequestBody request: AnomalyLabelRequestDto): ResponseEntity<LabelDto> {
+        return ResponseEntity.ok(anomalyService.labelAnomaly(request))
+    }
 }
 
