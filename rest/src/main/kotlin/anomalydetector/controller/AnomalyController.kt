883f1316fac587af06217f6bf825156bebd200a6
Jakub Klepacz <jakub.klepacz@tomtom.com>
2025-08-08 10:32:48 +0200

    feat: [AD-009] added cross origin in controller

--- rest/src/main/kotlin/anomalydetector/controller/AnomalyController.kt
+++ rest/src/main/kotlin/anomalydetector/controller/AnomalyController.kt
@@ -7,6 +7,7 @@ import anomalydetector.service.AnomalyService
 import anomalydetector.service.FeatureCollection
 import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/anomaly")
+@CrossOrigin(origins = ["*"])
 class AnomalyController(private val anomalyService: AnomalyService) {
     @Operation(summary = "Detect anomalies in provided data and time range")
     @PostMapping
